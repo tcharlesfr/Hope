9183b201f91d68e1fef98d7f05fdffcd983fef1d
tcharlesfr <tcharlesandroid@gmail.com>
2023-08-16 10:12:40 -0300

    css na home e adicionado a descrição

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -55,9 +55,8 @@ function Home() {
                       ></div>
                       <Card.Body>
                         <Card.Title>{post.name}</Card.Title>
-                        <Card.Text>
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
+                        <Card.Text className={styles.description}>
+                          {post.age}
                         </Card.Text>
                         <Button variant="primary" style={{ color: "white" }}>
                           {post.available ? (
